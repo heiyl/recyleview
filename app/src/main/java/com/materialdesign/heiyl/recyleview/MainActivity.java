5ae78ac0c47223733ffe24844cce4601be2de744
heiyulong <heiyulong@xdf.cn>
2018-05-07 17:29:46 +0800

    Initial commit

---
+++ app/src/main/java/com/materialdesign/heiyl/recyleview/MainActivity.java
@@ -0,0 +1,42 @@
+package com.materialdesign.heiyl.recyleview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView tv_recyleview_elementary;
+    private TextView tv_recyleview_diver;
+    private TextView tv_recyleview_drafting;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews(){
+        tv_recyleview_elementary = this.findViewById(R.id.tv_recyleview_elementary);
+        tv_recyleview_diver = this.findViewById(R.id.tv_recyleview_diver);
+        tv_recyleview_drafting = this.findViewById(R.id.tv_recyleview_drafting);
+
+        tv_recyleview_elementary.setOnClickListener(this);
+        tv_recyleview_diver.setOnClickListener(this);
+        tv_recyleview_drafting.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tv_recyleview_elementary://recyleview基本用法
+                break;
+            case R.id.tv_recyleview_diver://recyleview添加分割线
+                break;
+            case R.id.tv_recyleview_drafting://recyleview拖拽动画
+                break;
+        }
+    }
+}
