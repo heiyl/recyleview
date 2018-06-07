241757284d12f16ce859ba3b7e04bb4e508f1303
heiyulong <heiyulong@xdf.cn>
2018-06-07 11:18:01 +0800

    recyleview添加头部和底部功能封装

--- app/src/main/java/com/materialdesign/heiyl/recyleview/ui/activitys/RVHeaderActivity.java
+++ app/src/main/java/com/materialdesign/heiyl/recyleview/ui/activitys/RVHeaderActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.view.Gravity;
+import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.TextView;
 
@@ -24,37 +25,38 @@ public class RVHeaderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rvheader);
-
         recyclerView = (WrapRecyleView) findViewById(R.id.recyclerView);
-
-
-//		View headerView = View.inflate(this, resource, root);
-        TextView headerView = new TextView(this);
-        //		TextView tv = headerView.findViewById(id);
-        LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
-        headerView.setLayoutParams(params);
-        headerView.setGravity(Gravity.CENTER);
-        headerView.setPadding(25,25,25,25);
-        headerView.setBackgroundResource(android.R.color.holo_orange_light);
-        headerView.setText("HeaderView");
-        recyclerView.addHeaderView(headerView);
-
-        TextView footerView = new TextView(this);
-        params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
-        footerView.setLayoutParams(params);
-        footerView.setGravity(Gravity.CENTER);
-        footerView.setPadding(25,25,25,25);
-        footerView.setBackgroundResource(android.R.color.darker_gray);
-        footerView.setText("FooterView");
-        recyclerView.addFooterView(footerView);
+        addHeaderView();
+        addFooterView();
 
         List<String> list = new ArrayList<>();
         for (int i = 0; i < 6; i++) {
             list.add("item "+i);
         }
-
         MyAdapter adapter = new MyAdapter(list);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
+    }
+
+    private void addFooterView() {
+        TextView footerView = new TextView(this);
+        LayoutParams paramsFooter = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+        footerView.setLayoutParams(paramsFooter);
+        footerView.setGravity(Gravity.CENTER);
+        footerView.setPadding(25,25,25,25);
+        footerView.setBackgroundResource(android.R.color.darker_gray);
+        footerView.setText("FooterView");
+        recyclerView.addFooterView(footerView);
+    }
+
+    private void addHeaderView() {
+        TextView headerView = new TextView(this);
+        LayoutParams paramsHeader = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+        headerView.setLayoutParams(paramsHeader);
+        headerView.setGravity(Gravity.CENTER);
+        headerView.setPadding(25,25,25,25);
+        headerView.setBackgroundResource(android.R.color.holo_orange_light);
+        headerView.setText("HeaderView");
+        recyclerView.addHeaderView(headerView);
     }
 }
